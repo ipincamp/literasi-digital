325a32b696aaa285a9382916218e5ebe49ff152b
ipincamp <support@nur-arifin.my.id>
2025-10-27 13:26:43 +0700

    Refactor NilaiController to improve query structure and maintainability

--- app/Http/Controllers/NilaiController.php
+++ app/Http/Controllers/NilaiController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -16,20 +17,21 @@ class NilaiController extends Controller
             $jumlahSoal = DB::table('soal')->count();
 
             $data = DB::table('nilai')
-            ->join('users', 'users.id', '=', 'nilai.id_siswa')
-            ->select(
-                'users.id as id_siswa',
-                'users.name',
-                'nilai.percobaan',
-                DB::raw('SUM(nilai.nilai) as total_benar'),
-                DB::raw("($jumlahSoal) as jumlah_soal"),
-                DB::raw("(SUM(nilai.nilai) / $jumlahSoal) * 100 as total_nilai"),
-                DB::raw('MIN(nilai.created_at) as mulai'),
-                DB::raw('MAX(nilai.created_at) as selesai')
-            )
-            ->groupBy('nilai.id_siswa', 'users.name', 'nilai.percobaan')
-            ->orderByDesc('selesai')
-            ->get();
+                ->join('users', 'users.id', '=', 'nilai.id_siswa')
+                ->select(
+                    // 'users.id as id_siswa',
+                    'nilai.id_siswa',
+                    'users.name',
+                    'nilai.percobaan',
+                    DB::raw('SUM(nilai.nilai) as total_benar'),
+                    DB::raw("($jumlahSoal) as jumlah_soal"),
+                    DB::raw("(SUM(nilai.nilai) / $jumlahSoal) * 100 as total_nilai"),
+                    DB::raw('MIN(nilai.created_at) as mulai'),
+                    DB::raw('MAX(nilai.created_at) as selesai')
+                )
+                ->groupBy('nilai.id_siswa', 'users.name', 'nilai.percobaan')
+                ->orderByDesc('selesai')
+                ->get();
 
             return view('nilai.admin', compact('data'));
         } else {
@@ -37,18 +39,18 @@ class NilaiController extends Controller
             $jumlahSoal = DB::table('soal')->count();
 
             $data = DB::table('nilai')
-            ->where('id_siswa', Auth::id())
-            ->select(
-                'percobaan',
-                DB::raw("SUM(nilai) as total_benar"),
-                DB::raw("($jumlahSoal) as jumlah_soal"),
-                DB::raw("(SUM(nilai) / $jumlahSoal) * 100 as total_nilai"),
-                DB::raw('MIN(created_at) as mulai'),
-                DB::raw('MAX(created_at) as selesai')
-            )
-            ->groupBy('percobaan')
-            ->orderByDesc('selesai')
-            ->get();
+                ->where('id_siswa', Auth::id())
+                ->select(
+                    'percobaan',
+                    DB::raw("SUM(nilai) as total_benar"),
+                    DB::raw("($jumlahSoal) as jumlah_soal"),
+                    DB::raw("(SUM(nilai) / $jumlahSoal) * 100 as total_nilai"),
+                    DB::raw('MIN(created_at) as mulai'),
+                    DB::raw('MAX(created_at) as selesai')
+                )
+                ->groupBy('percobaan')
+                ->orderByDesc('selesai')
+                ->get();
 
             return view('nilai.siswa', compact('data'));
         }
